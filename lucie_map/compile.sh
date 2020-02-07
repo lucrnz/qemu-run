e847a0c72b623db5859ff0e968e2a0504f0e06f8
Lucie Cupcakes <cirno9moe@gmail.com>
2020-02-07 20:55:20 -0300

    fast_push at Fri 07 Feb 2020 08:55:20 PM -03

--- lucie_map/compile.sh
+++ lucie_map/compile.sh
@@ -4,6 +4,6 @@ CXXFLAGS="-O2 -s -mtune=generic"
 #CXXFLAGS="-g -O0 -mtune=generic"
 CXXSTD="-std=c++11 -pedantic -Werror"
 
-$CXX $CXXSTD $CXXFLAGS -static -static-libstdc++ -static-libgcc -c -fPIC  map.cc -o map.o || exit 1
+$CXX $CXXSTD $CXXFLAGS -static -static-libstdc++ -static-libgcc -c -fPIC map.cc -o map.o || exit 1
 ar rcs ../libmap.a map.o || exit 1
 rm map.o
