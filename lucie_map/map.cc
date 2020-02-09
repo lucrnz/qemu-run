a8dc8b270c9032604dfb7d0586f45a92cc8d8513
Lucie Cupcakes <cirno9moe@gmail.com>
2020-02-09 03:03:11 -0300

    fast_push at dom feb  9 03:03:11 -03 2020

--- lucie_map/map.cc
+++ lucie_map/map.cc
@@ -1,10 +1,11 @@
 #include <map>
+#include <iostream>
 #include "map.h"
 
 #define cpp_map \
-	std::map<char*, char*>
+	std::map<std::string, std::string>
 #define cpp_pair \
-	std::pair<char*, char*>
+	std::pair<std::string, std::string>
 
 extern "C" map* map_init() {
 	map* m = (map*) malloc(sizeof(map));
@@ -14,7 +15,7 @@ extern "C" map* map_init() {
 
 extern "C" void map_insert(map* _m, char* key, char* value) {
 	cpp_map* m = (cpp_map*) _m->_the_map;
-	m->insert(cpp_pair(key, value));
+	m->insert(cpp_pair(std::string(key), std::string(value)));
 }
 
 extern "C" int map_find(map* _m, char* key, char** value_ptr) {
@@ -22,8 +23,7 @@ extern "C" int map_find(map* _m, char* key, char** value_ptr) {
 	cpp_map m = *( (cpp_map*) _m->_the_map );
 	cpp_map::iterator it;
 
-	if( (it = m.find(key)) != m.end() ) {
-		*value_ptr = it->second;
+	if( (it = m.find(std::string(key))) != m.end() ) {
 		rc = 0;
 	}
 	return rc;
