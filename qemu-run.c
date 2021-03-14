a5e31b18278825f42fe3680ff261e0f93d9e55e0
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-03-14 00:05:20 -0300

    fast_commit at 20210314_000520

--- qemu-run.c
+++ qemu-run.c
@@ -27,9 +27,9 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #define cfg_add_kv(cfg, k, v) g_hash_table_insert(cfg, g_strdup(k), g_strdup(v));
 #define g_free_ifn_null(...) \
     do { \
-        int i=0;\
-        void* pta[] = {__VA_ARGS__}; \
-        for(i=0; i < sizeof(pta)/sizeof(void*); i++) { \
+        int i = 0;\
+        void *pta[] = {__VA_ARGS__}; \
+        for(i = 0; i < sizeof(pta)/sizeof(void*); i++) { \
 			void *x = pta[i]; \
             if (x != NULL) { \
 				g_free(x); \
@@ -43,12 +43,12 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
     printf("This software is Free software - released under the GPLv3 License.\n"); \
     printf("Read the LICENSE file. Or go visit https://www.gnu.org/licenses/gpl-3.0.html\n");
 
-gboolean file_exists (const char* fpath) {
+gboolean file_exists (const char *fpath) {
 	struct stat buffer;
 	return (stat (fpath, &buffer) == 0) ? TRUE : FALSE;
 }
 
-gboolean g_dir_exists(const gchar* path) {
+gboolean g_dir_exists(const gchar *path) {
 	gboolean rc = FALSE;
 	GDir* gd = g_dir_open(path, 0, NULL);
 	if (gd != NULL) {
@@ -58,11 +58,29 @@ gboolean g_dir_exists(const gchar* path) {
 	return rc;
 }
 
-void copy_char_chunk(const char* src_ptr, char* dest_ptr, size_t data_length) {
+gboolean g_hash_table_match_key_alow(GHashTable *t, gpointer k, const char *s) {
+	/* Returns TRUE if the value of the provided key for the HashTable t
+	 * at lowercase is equal to s
+	 * Otherwise, returns FALSE
+	*/
+	gboolean res = FALSE;
+	char *t_v_low;
+	gpointer t_v;
+	
+	if ( t_v = g_hash_table_lookup(t, k) != NULL) {
+		t_v_low = g_ascii_strdown(t_v, -1);		
+		res = strcmp((const char*)t_v_low, s) == 0 ? TRUE : FALSE;
+		g_free(cfg_v_low);
+	}
+	
+	return res;
+}
+
+void copy_char_chunk(const char *src_ptr, char *dest_ptr, size_t data_length) {
 	for (size_t i = 0; i < data_length; i++) *(dest_ptr + i) = *(src_ptr + i);
 }
 
-size_t cstr_trim_right(const char* cstr, const size_t length) {
+size_t cstr_trim_right(const char *cstr, const size_t length) {
 	size_t difference = 0;
 	for (size_t i = length - 1; i < length; i--) {
 		if (! isspace(*(cstr + i))) break;
@@ -71,14 +89,13 @@ size_t cstr_trim_right(const char* cstr, const size_t length) {
 	return length - difference;
 }
 
-int process_kv_pair(char* kv_cstr, GHashTable* cfg) {
+int process_kv_pair(char *kv_cstr, GHashTable *cfg) {
 	size_t kv_cstr_len = strlen(kv_cstr);
 	kv_cstr[kv_cstr_len - 1] = '\0';
 	if (kv_cstr_len < 3) return 1;
 
 	char c = '\0';
-	char value_buffer[BUFFER_MAX];
-	char key_buffer[BUFFER_MAX];
+	char value_buffer[BUFFER_MAX], key_buffer[BUFFER_MAX];
 
 	size_t value_len = 0, key_len = 0;
 	int setting_key = 1, trimming_left = 1;
@@ -115,18 +132,18 @@ int process_kv_pair(char* kv_cstr, GHashTable* cfg) {
 	return 0;
 }
 
-int config_load(const char* fpath, GHashTable* cfg) {
-	FILE* fptr = fopen(fpath, "r");
-	if (fptr == NULL) return 1; //@TODO: error management
+gboolean config_load(const char *fpath, GHashTable *cfg) {
+	FILE *fptr = fopen(fpath, "r");
+	if (fptr == NULL) return FALSE; //@TODO: error management
 	char line[BUFFER_MAX];
 	while(fgets(line, BUFFER_MAX, fptr) != NULL) {
 		process_kv_pair(line, cfg);
 	}
 	fclose(fptr);
-	return 0;
+	return TRUE;
 }
 
-void program_get_cfg_values(GHashTable* cfg, char* vm_dir) {
+void program_get_cfg_values(GHashTable *cfg, char *vm_dir) {
 	char nproc_str[4];
 	snprintf(nproc_str, 4, "%d", get_nprocs());
 	cfg_add_kv(cfg, "sys", "x64");
@@ -164,14 +181,54 @@ void program_get_cfg_values(GHashTable* cfg, char* vm_dir) {
 	}
 }
 
-void program_build_cmd_line(GHashTable* cfg, char* vm_dir) {
-	int drive_index = 0;
-    int telnet_port = 0;
-    char *sf_str = "";
-    char *fwd_ports_str = "";
+gboolean program_build_cmd_line(GHashTable *cfg, char *vm_dir, char *vm_name, GPtrArray **out_cmd) {
+	gboolean rc = TRUE;
+	int drive_index = 0, telnet_port = 0;
+	gpointer cfg_k, cfg_v;
+	*out_cmd = NULL;
+	cmd = g_ptr_array_new_with_free_func(g_free);
+	char cmd_slice_buff[BUFFER_MAX];
+	
+	cfg_v = g_hash_table_lookup_extended(cfg, "sys");
+	if (strcmp((const char*)cfg_v, "x32") == 0) {
+		g_ptr_array_add(cmd, g_strdup("qemu-system-i386"));
+	} else if (strcmp((const char*)cfg_v, "x64") == 0) {
+		g_ptr_array_add(cmd, g_strdup("qemu-system-x86_64"));
+	} else {
+		log_msg("Invalid value for sys"); //@TODO: Logger
+		g_ptr_array_free(cmd, TRUE);
+		rc = FALSE;
+	}
+
+	if (g_hash_table_match_key_alow(cfg, "acc", "yes")) {
+		g_ptr_array_add(cmd, g_strdup("--enable-kvm"));
+	}
+	
+	g_ptr_array_add(cmd, g_strdup("-smp"));
+	g_ptr_array_add(cmd, g_strdup((char*)g_hash_table_lookup(cfg, "cores")));
+	g_ptr_array_add(cmd, g_strdup("-m"));
+	g_ptr_array_add(cmd, g_strdup((char*)g_hash_table_lookup(cfg, "mem")));
+	g_ptr_array_add(cmd, g_strdup("-boot"));
+	snprintf(cmd_slice_buff, BUFFER_MAX, "order=%s", vm_dir, (char*)g_hash_table_lookup(cfg, "boot"));
+	g_ptr_array_add(cmd, g_strdup(cmd_slice_buff));
+
+	g_ptr_array_add(cmd, g_strdup("-usb"));
+	g_ptr_array_add(cmd, g_strdup("-device"));
+	g_ptr_array_add(cmd, g_strdup("usb-tablet"));
+	
+	if (! g_hash_table_match_key_alow(cfg, "snd", "no")) {
+		g_ptr_array_add(cmd, g_strdup("--soundhw"));
+		g_ptr_array_add(cmd, g_strdup((char*)g_hash_table_lookup(cfg, "snd")));
+	}
+
+	if (rc) {
+		*out_cmd = cmd;
+	}
+	
+	return rc;
 }
 
-gboolean program_find_vm_location(int argc, char** argv, char** out_vm_name, char** out_vm_dir, char** out_vm_cfg_file) {
+gboolean program_find_vm_location(int argc, char **argv, char **out_vm_name, char **out_vm_dir, char **out_vm_cfg_file) {
 	gboolean rc = FALSE;
 	*out_vm_name = NULL;
 	*out_vm_dir = NULL;
@@ -191,11 +248,11 @@ gboolean program_find_vm_location(int argc, char** argv, char** out_vm_name, cha
 		*out_vm_name = g_strdup("VM_NAME");*/
 	} else {
 		// Normal lookup using ENV var
-		const char* vm_name = argv[1];
-		const char* vm_dir_env_str = getenv("QEMURUN_VM_PATH");
+		const char *vm_name = argv[1];
+		const char *vm_dir_env_str = getenv("QEMURUN_VM_PATH");
 		gboolean vm_dir_exists = FALSE;
 		char vm_dir[PATH_MAX];
-		gchar** vm_dir_env = g_strsplit(vm_dir_env_str, ":", 0);
+		gchar **vm_dir_env = g_strsplit(vm_dir_env_str, ":", 0);
 
 		for (int i = 0; vm_dir_env[i] != NULL && vm_dir_exists == FALSE; i++) {
 			snprintf(vm_dir, PATH_MAX, "%s/%s", vm_dir_env[i], vm_name);
@@ -222,11 +279,11 @@ void g_hash_table_print(gpointer key, gpointer value, gpointer user_data) {
 	printf("%s=%s\n", (char*) key, (char*) value);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 	print_gpl_banner();
-	char* vm_name = NULL;
-	char* vm_dir = NULL;
-	char* vm_cfg_file = NULL;
+	char *vm_name = NULL;
+	char *vm_dir = NULL;
+	char *vm_cfg_file = NULL;
 	
 	if (! program_find_vm_location(argc, argv, &vm_name, &vm_dir, &vm_cfg_file)) {
 		return 1;
@@ -234,9 +291,12 @@ int main(int argc, char** argv) {
 	
 	//printf("vm_name=%s\nvm_dir=%s\nvm_cfg_file=%s\n", vm_name, vm_dir, vm_cfg_file);
 	
-	GHashTable* cfg = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
+	GHashTable *cfg = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
 	program_get_cfg_values(cfg, vm_dir);
-	config_load(vm_cfg_file, cfg);
+	if (! config_load(vm_cfg_file, cfg)) {
+		log_msg("Error: cannot load config.");
+		return 1;
+	}
 
 	/*gpointer k;
 	gpointer v;
