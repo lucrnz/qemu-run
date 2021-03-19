0786f1b16198ca33ff2d9f8afb2e2c6074034da6
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-03-19 04:34:11 -0300

    fast_commit at 20210319_043411

--- qemu-run.c
+++ qemu-run.c
@@ -16,20 +16,20 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #include <ctype.h>
 #include <unistd.h>
 #include <linux/limits.h>
-#include <sys/sysinfo.h> // @TODO: Is this Linux only ?
+#include <sys/sysinfo.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include "hashmap.h"
 
-#define buff_size_slice 1024
-#define buff_size_max buff_size_slice*1024
+#define buff_size_slice 128
+#define buff_size_max buff_size_slice*32
 
-#define log_msg(m) fprintf(stderr, "%s\n", m)
-
-#define hashmap_put_lk(m, k, val) \
-	hashmap_put(m, k, sizeof(k)-1, val) // where k is str literal
-#define hashmap_get_lk(m, k) \
-	hashmap_get(m, k, sizeof(k)-1) // where k is str literal
+#define hashmap_put_lk(m, lk, val) \
+	hashmap_put(m, lk, sizeof(lk)-1, val) // where lk is str literal
+#define hashmap_get_lk(m, lk) \
+	hashmap_get(m, lk, sizeof(lk)-1) // where k is str literal
+#define hashmap_match_char_lka(t, lk, s) \
+	(strcasecmp((const char*)hashmap_get(t, lk, sizeof(lk)-1), s) == 0 ? 1 : 0)
 
 #define puts_gpl_banner() \
 	puts("qemu-run. Forever beta software. Use on production on your own risk!\nThis software is Free software - released under the GPLv3 License.\nRead the LICENSE file. Or go visit https://www.gnu.org/licenses/gpl-3.0.html\n")
@@ -70,80 +70,52 @@ int filetype(const char *fpath) {
 	return ret;
 }
 
-bool hashmap_match_char_ka(struct hashmap_s *t, char* k, const char *s) {
-	bool res = 0;
-	void* v = hashmap_get(t, k, strlen(k));
-	if (v != NULL)
-		res = strcasecmp((const char*)v, s) == 0 ? 1 : 0;
-	return res;
-}
-
-size_t cstr_trim_right(const char *cstr, const size_t length) {
-	size_t difference = 0;
-	for (size_t i = length - 1; i < length; i--) {
-		if (! isspace(*(cstr + i))) break;
-		difference++;
+size_t cstr_trim_right(const char *cstr, const size_t len) {
+	size_t diff = 0;
+	for (size_t i = len - 1; i < len; i--) {
+		if (! isspace((int)cstr[i])) break;
+		diff++;
 	}
-	return length - difference;
+	return len - diff;
 }
 
 bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char delim) {
-	size_t kv_cstr_len = strlen(kv_cstr);
-	kv_cstr[kv_cstr_len - 1] = '\0';
-	if (kv_cstr_len < 3) return 0;
-
-	char c = '\0';
-	char value_buffer[buff_size_slice], key_buffer[buff_size_slice];
-
-	size_t value_len = 0, key_len = 0;
-	int setting_key = 1, trimming_left = 1;
-
-	for (size_t i = 0; (c = kv_cstr[i]) != '\0'; i++) {
-		if (c == delim) {
-			setting_key = 0;
-			trimming_left = 1;
-			continue;
-		}
+	bool rc = 0, setting_key = 1, trimming_left = 1;
+	char val_buff[buff_size_slice], key_buff[buff_size_slice], *key_ptr, *val_ptr;
+	size_t val_len = 0, key_len = 0;
+	for (char c = 0; (c = *kv_cstr) != '\0'; kv_cstr++) {
+		if (c == delim) { setting_key = 0; trimming_left = 1; continue; }
 		if (setting_key && trimming_left && !isspace(c)) {
-			trimming_left = 0;
-			key_buffer[key_len] = c;
-			key_len++;
+			trimming_left = 0; key_buff[key_len] = c; key_len++;
 		} else if (setting_key && !trimming_left)  {
-			key_buffer[key_len] = c;
-			key_len++;
+			key_buff[key_len] = c; key_len++;
 		} else if (!setting_key && trimming_left && !isspace(c)) {
-			trimming_left = 0;
-			value_buffer[value_len] = c;
-			value_len++;
+			trimming_left = 0; val_buff[val_len] = c; val_len++;
 		} else if (!setting_key && !trimming_left) {
-			value_buffer[value_len] = c;
-			value_len++;
+			val_buff[val_len] = c; val_len++;
 		}
 	}
-	key_buffer[key_len] = 0;
-	value_buffer[value_len] = 0;
-	key_len = cstr_trim_right(key_buffer, key_len);
-	value_len = cstr_trim_right(value_buffer, value_len);
-	key_buffer[key_len] = 0;
-	value_buffer[value_len] = 0;
-	
-	char *key_ptr, *val_ptr;
-	key_ptr = *str_pool;
-	append_to_strpool(str_pool, key_buffer);
-	val_ptr = *str_pool;
-	append_to_strpool(str_pool, value_buffer);
-	
-	hashmap_put(cfg, key_ptr, key_len, val_ptr);
-	
-	return 1;
+	if (! setting_key) { // If we actually did set a value.
+		key_buff[key_len] = '\0'; val_buff[val_len] = '\0';
+		key_len = cstr_trim_right(key_buff, key_len);
+		val_len = cstr_trim_right(val_buff, val_len);
+		key_buff[key_len] = '\0'; val_buff[val_len] = '\0';
+		key_ptr = *str_pool; append_to_strpool(str_pool, key_buff);
+		val_ptr = *str_pool; append_to_strpool(str_pool, val_buff);
+		rc = (hashmap_put(cfg, key_ptr, key_len, val_ptr) != 0);
+	}
+	return rc;
 }
 
-bool program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpath) {
+bool program_load_config(char *err, struct hashmap_s *cfg, char **str_pool, const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
-	if (fptr == NULL) return 0; //@TODO: error management
+	if (fptr == NULL) {
+		strcpy(err, "Cannot open config file. Check file permissions?");
+		return 0;
+	}
 	char line[buff_size_slice*2];
-	while(fgets(line, buff_size_slice, fptr) != NULL) {
-		// @TODO: replace with strtok
+	while(fgets(line, buff_size_slice*2, fptr) != NULL) {
+		line[strlen(line)-1] = '\0'; // Remove EOF or Newline char
 		process_kv_pair(line, cfg, str_pool, '=');
 	}
 	fclose(fptr);
@@ -151,8 +123,7 @@ bool program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpa
 }
 
 bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char *vm_dir) {
-	char path_buff[PATH_MAX], nproc_str[4];
-	char* val_tmp;
+	char path_buff[PATH_MAX], nproc_str[4], *val_tmp;
 	snprintf(nproc_str, 4, "%d", get_nprocs());
 	add_cfg_val(val_tmp, str_pool, cfg, "sys", "x64");
 	add_cfg_val(val_tmp, str_pool, cfg, "efi", "no");
@@ -172,7 +143,6 @@ bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	add_cfg_val(val_tmp, str_pool, cfg, "headless", "no");
 	add_cfg_val(val_tmp, str_pool, cfg, "vnc_pwd", "");
 	add_cfg_val(val_tmp, str_pool, cfg, "monitor_port", "5510");
-
 	snprintf(path_buff, PATH_MAX, "%s/shared", vm_dir);
 	add_cfg_val(val_tmp, str_pool, cfg, "shared", path_is_dir(path_buff) ? path_buff : "");
 	snprintf(path_buff, PATH_MAX, "%s/floppy", vm_dir);
@@ -184,32 +154,32 @@ bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	return 1;
 }
 
-bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
+bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
 	bool rc = 1;
 	int drive_index = 0, telnet_port = 55555; // @TODO: Get usable TCP port
 	void* cfg_v;
 	char cmd_slice[buff_size_slice] = {0};
 
 	bool vm_has_name = (strcmp(vm_name, "") != 0 ? 1 : 0);
-	bool vm_has_acc_enabled = hashmap_match_char_ka(cfg, "acc", "yes");
-	bool vm_has_vncpwd = (strcmp((const char*)hashmap_get_lk(cfg, "vnc_pwd"), "") != 0 ? 1 : 0);
-	bool vm_has_audio = (hashmap_match_char_ka(cfg, "snd", "no") ? 1 : 0);
-	bool vm_has_videoacc = hashmap_match_char_ka(cfg, "host_video_acc", "yes");
-	bool vm_has_rngdev = hashmap_match_char_ka(cfg, "rng_dev", "yes");
-	bool vm_is_headless = hashmap_match_char_ka(cfg, "headless", "yes");
-	bool vm_clock_is_localtime = hashmap_match_char_ka(cfg, "localtime", "yes");
+	bool vm_has_acc_enabled = hashmap_match_char_lka(cfg, "acc", "yes");
+	bool vm_has_vncpwd = (strcmp(hashmap_get_lk(cfg, "vnc_pwd"), "") != 0 ? 1 : 0);
+	bool vm_has_audio = hashmap_match_char_lka(cfg, "snd", "no");
+	bool vm_has_videoacc = hashmap_match_char_lka(cfg, "host_video_acc", "yes");
+	bool vm_has_rngdev = hashmap_match_char_lka(cfg, "rng_dev", "yes");
+	bool vm_is_headless = hashmap_match_char_lka(cfg, "headless", "yes");
+	bool vm_clock_is_localtime = hashmap_match_char_lka(cfg, "localtime", "yes");
 	bool vm_has_sharedf = (strcmp(hashmap_get_lk(cfg, "shared"), "") != 0 ? 1 : 0);
-	bool vm_has_hddvirtio = hashmap_match_char_ka(cfg, "hdd_virtio", "yes");
-	
+	bool vm_has_hddvirtio = hashmap_match_char_lka(cfg, "hdd_virtio", "yes");
+	bool vm_has_network = (strcmp(hashmap_get_lk(cfg, "net"), "") != 0 ? 1 : 0);
 	vm_has_sharedf = vm_has_sharedf ? path_is_dir(hashmap_get_lk(cfg, "shared")) : 0;
-	
+
 	cfg_v = hashmap_get_lk(cfg, "sys");
 	if (strcmp((const char*)cfg_v, "x32") == 0) {
 		out_cmd = append_to_cmd(out_cmd, "qemu-system-i386");
 	} else if (strcmp((const char*)cfg_v, "x64") == 0) {
 		out_cmd = append_to_cmd(out_cmd, "qemu-system-x86_64");
 	} else {
-		log_msg("Invalid value for sys"); //@TODO: Logger
+		strcpy(err, "Invalid value for sys");
 		return 0;
 	}
 	
@@ -235,14 +205,33 @@ bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 		out_cmd = append_to_cmd(out_cmd, vm_has_videoacc ? "-display gtk,gl=on" : "-display gtk,gl=off");
 	}
 	
-	// @TODO: Forward ports logic
-	snprintf(cmd_slice, buff_size_slice, "%s-nic user,model=%s%s%s",
-		vm_has_rngdev ? "-object rng-random,id=rng0,filename=/dev/random -device virtio-rng-pci,rng=rng0 " : "",
-		(char*)hashmap_get_lk(cfg, "net"),
-		vm_has_sharedf ? ",smb=" : "",
-		vm_has_sharedf ? (char*)hashmap_get_lk(cfg, "shared") : ""
-	);
-	out_cmd = append_to_cmd(out_cmd, cmd_slice);
+	if (vm_has_network) {
+		snprintf(cmd_slice, buff_size_slice, "-nic user,model=%s%s%s",
+			(char*)hashmap_get_lk(cfg, "net"),
+			vm_has_sharedf ? ",smb=" : "",
+			vm_has_sharedf ? (char*)hashmap_get_lk(cfg, "shared") : ""
+		);
+		out_cmd = append_to_char_arr(out_cmd, cmd_slice, 0, 0, 0);
+		cfg_v = hashmap_get_lk(cfg, "fwd_ports");
+		bool vm_has_fwd_ports = (strcmp((const char*) cfg_v, "no") != 0);
+		if (! vm_has_fwd_ports) {
+			out_cmd = append_to_char_arr(out_cmd, " ", 0, 0, 0);
+		} else {
+			char* cfg_v_c = (char*)cfg_v;
+			if (strstr(cfg_v_c, ":") != NULL) { // If have fwd_ports=<HostPort>:<GuestPort>
+				char *fwd_ports_tk = strtok(cfg_v_c, ":");
+				char fwd_port_a[16], fwd_port_b[16];
+				for (int i = 0; fwd_ports_tk != NULL && i<2; i++) {
+					strcpy(i == 0 ? fwd_port_a : fwd_port_b, fwd_ports_tk);
+					fwd_ports_tk = strtok(NULL, ":");
+				}
+				snprintf(cmd_slice, buff_size_slice, ",hostfwd=tcp::%s-:%s,hostfwd=udp::%s-:%s", fwd_port_a, fwd_port_b, fwd_port_a, fwd_port_b);
+			} else { // Else use the same port for Host and Guest.
+				snprintf(cmd_slice, buff_size_slice, ",hostfwd=tcp::%s-:%s,hostfwd=udp::%s-:%s", cfg_v_c, cfg_v_c, cfg_v_c, cfg_v_c);
+			}
+			out_cmd = append_to_cmd(out_cmd, cmd_slice);
+		}
+	}
 	
 	cfg_v = hashmap_get_lk(cfg, "floppy");
 	if (path_is_file((const char*) cfg_v)) {
@@ -264,20 +253,20 @@ bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 		out_cmd = append_to_cmd(out_cmd, cmd_slice);
 		drive_index++;
 	}
-	
-	if (vm_clock_is_localtime) append_to_char_arr(out_cmd, "-rtc base=localtime", 1, 0, 0);
 
+	if (vm_has_rngdev) out_cmd = append_to_cmd(out_cmd, "-object rng-random,id=rng0,filename=/dev/random -device virtio-rng-pci,rng=rng0");
+	if (vm_clock_is_localtime) out_cmd = append_to_char_arr(out_cmd, "-rtc base=localtime", 1, 0, 0);
 	return rc;
 }
 
-bool program_find_vm_location(int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
+bool program_find_vm_location(char *err, int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
 	bool rc = 0, vm_dir_exists = 0;
 	char *vm_name, vm_dir[buff_size_slice-16];
 
 	if (argc >= 1) {
 		vm_name = argv[1];
 	} else {
-		log_msg("Invalid argument count.");
+		strcpy(err, "Invalid argument count. Did you specified the VM Name?");
 		return 0;
 	}
 	
@@ -299,27 +288,30 @@ bool program_find_vm_location(int argc, char **argv, char *out_vm_name, char *ou
 		strcpy(out_vm_cfg_file, cfg_file);
 		rc = 1;
 	} else {
-		log_msg("Error: Cannot find VM, Check your VM_PATH env. variable ?");
+		strcpy(err, "Cannot find VM, Check your QEMURUN_VM_PATH env. variable ?");
 	}
 
 	return rc;
 }
 
 int main(int argc, char **argv) {
-	char cmd[buff_size_max], str_pool_d[buff_size_max], vm_name[buff_size_slice], vm_dir[PATH_MAX-16], vm_cfg_file[buff_size_slice];
-	char *str_pool = &str_pool_d[0];
+	char cmd[buff_size_max], err[buff_size_slice], str_pool_d[buff_size_max];
+	char vm_name[buff_size_slice], vm_dir[PATH_MAX-16], vm_cfg_file[buff_size_slice];
+	char *err_p = &err[0], *str_pool = &str_pool_d[0];
 	struct hashmap_s cfg;
 	puts_gpl_banner();
 	if (hashmap_create(16, &cfg) == 0 &&
-		program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file) && 
+		program_find_vm_location(err_p, argc, argv, vm_name, vm_dir, vm_cfg_file) &&
 		program_set_default_cfg_values(&cfg, &str_pool, vm_dir) &&
-		program_load_config(&cfg, &str_pool, vm_cfg_file) &&
-		program_build_cmd_line(&cfg, vm_name, cmd))
+		program_load_config(err_p, &cfg, &str_pool, vm_cfg_file) &&
+		program_build_cmd_line(err_p, &cfg, vm_name, cmd))
 	{
-		printf("Command line arguments:\n%s\n", cmd);
+		puts("Command line arguments:");
+		puts(cmd);
 		system(cmd);
 	} else {
-		log_msg("There was an error in the program.");
+		puts("There was an error in the program:");
+		puts(err);
 	}
 	hashmap_destroy(&cfg);
 	return 0;
