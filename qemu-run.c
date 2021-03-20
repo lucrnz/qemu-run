c0a5307cf59752837f5d5c34651081f1c085fdd1
Luis (Laffin) Espinoza <laffintoo@gmail.com>
2021-03-20 10:36:25 -0700

    Moved Errs into a fatal function

--- qemu-run.c
+++ qemu-run.c
@@ -36,7 +36,6 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #define dprint()
 #endif // DEBUG
 
-
 #define buff_size_slice 128
 #define buff_size_max buff_size_slice*32
 
@@ -61,6 +60,21 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 	val_tmp_ptr = *str_pool_ptr; \
 	append_to_strpool(str_pool_ptr, val); \
 	hashmap_put_lk(cfg, lkey, val_tmp_ptr)
+
+enum {ERR_UNKOWN,ERR_ARGS,ERR_ENV,ERR_CONFIG,ERR_SYS,ERR_EXEC,ERR_ENDLIST};
+char *errs[]={
+   "Invalid argument count. Did you specified the VM Name?",
+   "Cannot find VM, Check your QEMURUN_VM_PATH env. variable ?",
+   "Cannot open config file. Check file permissions?",
+   "Invalid value for sys",
+   "There was an error trying to execute qemu. Do you have it installed?."
+};
+
+void fatal(unsigned int errcode)
+{
+   printf("There was an error in the program:\n\t%s\n",errs[errcode<ERR_ENDLIST?errcode:0]);
+   exit(1);
+}
 
 char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool append_endchar, char endchar) {
 	char c; bool stop = 0;
@@ -125,13 +139,11 @@ bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char
 	return rc;
 }
 
-bool program_load_config(char *err, struct hashmap_s *cfg, char **str_pool, const char *fpath) {
+bool program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
 	dprint();
-	if (fptr == NULL) {
-		strcpy(err, "Cannot open config file. Check file permissions?");
-		return 0;
-	}
+	if (fptr == NULL)
+      fatal(ERR_CONFIG);
 	char line[buff_size_slice*2];
 	while(fgets(line, buff_size_slice*2, fptr) != NULL) {
 		line[strlen(line)-1] = '\0'; // Remove EOF or Newline char
@@ -178,7 +190,7 @@ bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	return 1;
 }
 
-bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
+bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
 	bool rc = 1;
 	int drive_index = 0, telnet_port = 55555; // @TODO: Get usable TCP port
 	void* cfg_v;
@@ -202,10 +214,8 @@ bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, cha
 		out_cmd = append_to_cmd(out_cmd, "qemu-system-i386");
 	} else if (strcmp((const char*)cfg_v, "x64") == 0) {
 		out_cmd = append_to_cmd(out_cmd, "qemu-system-x86_64");
-	} else {
-		strcpy(err, "Invalid value for sys");
-		return 0;
-	}
+	} else
+      fatal(ERR_SYS);
 
 	snprintf(cmd_slice, buff_size_slice, "%s-name %s -cpu %s -smp %s -m %s -boot order=%s -usb -device usb-tablet -vga %s %s%s",
 		vm_has_acc_enabled ? "--enable-kvm " : "",
@@ -283,17 +293,15 @@ bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, cha
 	return rc;
 }
 
-bool program_find_vm_location(char *err, int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
+bool program_find_vm_location(int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
 	bool rc = 0, vm_dir_exists = 0;
 	char *vm_name, vm_dir[PATH_MAX+1];
 
    dprint();
 	if (argc > 1) {
 		vm_name = argv[1];
-	} else {
-		strcpy(err, "Invalid argument count. Did you specified the VM Name?");
-		return 0;
-	}
+	} else
+      fatal(ERR_ARGS);
 
 	char vm_dir_env_str[PATH_MAX*16],*env;
 
@@ -312,34 +320,28 @@ bool program_find_vm_location(char *err, int argc, char **argv, char *out_vm_nam
 		strcpy(out_vm_dir, vm_dir);
 		strcpy(out_vm_cfg_file, cfg_file);
 		rc = 1;
-	} else {
-		strcpy(err, "Cannot find VM, Check your QEMURUN_VM_PATH env. variable ?");
-	}
-
+	} else
+      fatal(ERR_ENV);
 	return rc;
 }
 
 int main(int argc, char **argv) {
-	char cmd[buff_size_max], err[buff_size_slice], str_pool_d[buff_size_max];
-	char vm_name[buff_size_slice], vm_dir[PATH_MAX-16], vm_cfg_file[buff_size_slice];
+	char cmd[buff_size_max], str_pool_d[buff_size_max];
+	char vm_name[buff_size_slice], vm_dir[PATH_MAX+1], vm_cfg_file[buff_size_slice];
 	char *str_pool = &str_pool_d[0];
 	struct hashmap_s cfg;
 	dprint();
 	puts_gpl_banner();
 	if (hashmap_create(16, &cfg) == 0 &&
-		program_find_vm_location(err, argc, argv, vm_name, vm_dir, vm_cfg_file) &&
+		program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file) &&
 		program_set_default_cfg_values(&cfg, &str_pool, vm_dir) &&
-		program_load_config(err, &cfg, &str_pool, vm_cfg_file) &&
-		program_build_cmd_line(err, &cfg, vm_name, cmd))
+		program_load_config(&cfg, &str_pool, vm_cfg_file) &&
+		program_build_cmd_line(&cfg, vm_name, cmd))
 	{
 		puts("QEMU Command line arguments:");
 		puts(cmd);
-		if (system(cmd) == -1) {
-			puts("There was an error trying to execute qemu. Do you have it installed?.");
-		}
-	} else {
-		puts("There was an error in the program:");
-		puts(err);
+		if (system(cmd) == -1)
+         fatal(ERR_EXEC);
 	}
 	hashmap_destroy(&cfg);
 	return 0;
