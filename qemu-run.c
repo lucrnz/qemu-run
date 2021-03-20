aab534eafcfebfb964a2b05b02427940d8032bf7
Luis (Laffin) Espinoza <laffintoo@gmail.com>
2021-03-20 04:45:22 -0700

    Redid filetype\nAdded Debugging print\nFixed program_find_vm_location - Segfaulted on arg count & non exisyant emvironment variable

--- qemu-run.c
+++ qemu-run.c
@@ -16,15 +16,26 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #include <ctype.h>
 #include <unistd.h>
 #if defined(__WIN32__) || defined(__WIN64__) || defined(__WINNT__) || defined(__DOS__)
+#define PSEP ";"
+#define DSEP "\\"
 #include <limits.h>
 #else
 #define __NIX__
+#define PSEP ":"
+#define DSEP "/"
 #include <sys/sysinfo.h>
 #include <linux/limits.h>
 #endif
 #include <sys/stat.h>
 #include <sys/types.h>
 #include "hashmap.h"
+
+#ifdef DEBUG
+#define dprint() printf("%s (%d) %s\n",__FILE__,__LINE__,__FUNCTION__);
+#else
+#define dprint()
+#endif // DEBUG
+
 
 #define buff_size_slice 128
 #define buff_size_max buff_size_slice*32
@@ -38,9 +49,6 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 
 #define puts_gpl_banner() \
 	puts("qemu-run. Forever beta software. Use on production on your own risk!\nThis software is Free software - released under the GPLv3 License.\nRead the LICENSE file. Or go visit https://www.gnu.org/licenses/gpl-3.0.html\n")
-
-#define path_is_dir(p) (filetype(p) == 1 ? 1 : 0)
-#define path_is_file(p) (filetype(p) == 2 ? 1 : 0)
 
 #define append_to_strpool(strpool_ptr, val) do { \
 		*strpool_ptr = append_to_char_arr(*strpool_ptr, val, 1, 0, 0); \
@@ -56,6 +64,7 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 
 char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool append_endchar, char endchar) {
 	char c; bool stop = 0;
+	dprint();
 	while (!stop) {
 		c = *src; src++;
 		if (c == '\0' && append_strterm) stop=1;
@@ -66,17 +75,20 @@ char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool a
 	return dst;
 }
 
-int filetype(const char *fpath) {
+enum {FT_TYPE,FT_UNKNOWN=0,FT_PATH,FT_FILE};
+int filetype(const char *fpath,int type) {
 	int ret = 0; struct stat sb;
+	dprint();
 	if ((ret=(access (fpath,0) == 0))) {
 		stat(fpath,&sb);
-		ret+=S_ISREG(sb.st_mode)>0;
+		ret=S_ISREG(sb.st_mode)>0?FT_FILE:(S_ISDIR(sb.st_mode)?FT_PATH:FT_UNKNOWN);
 	}
-	return ret;
+	return ret=(type)?ret==type:ret;
 }
 
 size_t cstr_trim_right(const char *cstr, const size_t len) {
 	size_t diff = 0;
+	dprint();
 	for (size_t i = len - 1; i < len; i--) {
 		if (! isspace((int)cstr[i])) break;
 		diff++;
@@ -88,6 +100,7 @@ bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char
 	bool rc = 0, setting_key = 1, trimming_left = 1;
 	char val_buff[buff_size_slice], key_buff[buff_size_slice], *key_ptr, *val_ptr;
 	size_t val_len = 0, key_len = 0;
+	dprint();
 	for (char c = 0; (c = *kv_cstr) != '\0'; kv_cstr++) {
 		if (c == delim) { setting_key = 0; trimming_left = 1; continue; }
 		if (setting_key && trimming_left && !isspace(c)) {
@@ -114,6 +127,7 @@ bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char
 
 bool program_load_config(char *err, struct hashmap_s *cfg, char **str_pool, const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
+	dprint();
 	if (fptr == NULL) {
 		strcpy(err, "Cannot open config file. Check file permissions?");
 		return 0;
@@ -129,6 +143,7 @@ bool program_load_config(char *err, struct hashmap_s *cfg, char **str_pool, cons
 
 bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char *vm_dir) {
 	char path_buff[PATH_MAX], nproc_str[4], *val_tmp;
+	dprint();
 #ifdef __NIX__
 	snprintf(nproc_str, 4, "%d", get_nprocs());
 #else
@@ -153,13 +168,13 @@ bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	add_cfg_val(val_tmp, str_pool, cfg, "vnc_pwd", "");
 	add_cfg_val(val_tmp, str_pool, cfg, "monitor_port", "5510");
 	snprintf(path_buff, PATH_MAX, "%s/shared", vm_dir);
-	add_cfg_val(val_tmp, str_pool, cfg, "shared", path_is_dir(path_buff) ? path_buff : "");
+	add_cfg_val(val_tmp, str_pool, cfg, "shared", filetype(path_buff,FT_PATH) ? path_buff : "");
 	snprintf(path_buff, PATH_MAX, "%s/floppy", vm_dir);
-	add_cfg_val(val_tmp, str_pool, cfg, "floppy", path_is_file(path_buff) ? path_buff : "");
+	add_cfg_val(val_tmp, str_pool, cfg, "floppy", filetype(path_buff,FT_FILE) ? path_buff : "");
 	snprintf(path_buff, PATH_MAX, "%s/cdrom", vm_dir);
-	add_cfg_val(val_tmp, str_pool, cfg, "cdrom", path_is_file(path_buff) ? path_buff : "");
+	add_cfg_val(val_tmp, str_pool, cfg, "cdrom", filetype(path_buff,FT_FILE) ? path_buff : "");
 	snprintf(path_buff, PATH_MAX, "%s/disk", vm_dir);
-	add_cfg_val(val_tmp, str_pool, cfg, "disk", path_is_file(path_buff) ? path_buff : "");
+	add_cfg_val(val_tmp, str_pool, cfg, "disk", filetype(path_buff,FT_FILE) ? path_buff : "");
 	return 1;
 }
 
@@ -168,7 +183,7 @@ bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, cha
 	int drive_index = 0, telnet_port = 55555; // @TODO: Get usable TCP port
 	void* cfg_v;
 	char cmd_slice[buff_size_slice] = {0};
-
+   dprint();
 	bool vm_has_name = (strcmp(vm_name, "") != 0 ? 1 : 0);
 	bool vm_has_acc_enabled = hashmap_match_char_lka(cfg, "acc", "yes");
 	bool vm_has_vncpwd = (strcmp(hashmap_get_lk(cfg, "vnc_pwd"), "") != 0 ? 1 : 0);
@@ -180,7 +195,7 @@ bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, cha
 	bool vm_has_sharedf = (strcmp(hashmap_get_lk(cfg, "shared"), "") != 0 ? 1 : 0);
 	bool vm_has_hddvirtio = hashmap_match_char_lka(cfg, "hdd_virtio", "yes");
 	bool vm_has_network = (strcmp(hashmap_get_lk(cfg, "net"), "") != 0 ? 1 : 0);
-	vm_has_sharedf = vm_has_sharedf ? path_is_dir(hashmap_get_lk(cfg, "shared")) : 0;
+	vm_has_sharedf = vm_has_sharedf ? filetype(hashmap_get_lk(cfg, "shared"),FT_PATH) : 0;
 
 	cfg_v = hashmap_get_lk(cfg, "sys");
 	if (strcmp((const char*)cfg_v, "x32") == 0) {
@@ -243,21 +258,21 @@ bool program_build_cmd_line(char *err, struct hashmap_s *cfg, char *vm_name, cha
 	}
 
 	cfg_v = hashmap_get_lk(cfg, "floppy");
-	if (path_is_file((const char*) cfg_v)) {
+	if (filetype((const char*) cfg_v,FT_FILE)) {
 		snprintf(cmd_slice, buff_size_slice, "-drive index=%d,file=%s,if=floppy,format=raw", drive_index, (char*)cfg_v);
 		out_cmd = append_to_cmd(out_cmd, cmd_slice);
 		drive_index++;
 	}
 
 	cfg_v = hashmap_get_lk(cfg, "cdrom");
-	if (path_is_file((const char*) cfg_v)) {
+	if (filetype((const char*) cfg_v,FT_FILE)) {
 		snprintf(cmd_slice, buff_size_slice, "-drive index=%d,file=%s,media=cdrom", drive_index, (char*)cfg_v);
 		out_cmd = append_to_cmd(out_cmd, cmd_slice);
 		drive_index++;
 	}
 
 	cfg_v = hashmap_get_lk(cfg, "disk");
-	if (path_is_file((const char*) cfg_v)) {
+	if (filetype((const char*) cfg_v,FT_FILE)) {
 		snprintf(cmd_slice, buff_size_slice, "-drive index=%d,file=%s%s", drive_index, (char*)cfg_v, vm_has_hddvirtio ? ",if=virtio" : "");
 		out_cmd = append_to_cmd(out_cmd, cmd_slice);
 		drive_index++;
@@ -272,31 +287,35 @@ bool program_find_vm_location(char *err, int argc, char **argv, char *out_vm_nam
 	bool rc = 0, vm_dir_exists = 0;
 	char *vm_name, vm_dir[buff_size_slice-16];
 
-	if (argc >= 1) {
+	if (argc > 1) {
 		vm_name = argv[1];
 	} else {
 		strcpy(err, "Invalid argument count. Did you specified the VM Name?");
 		return 0;
 	}
+   dprint();
+	char vm_dir_env_str[buff_size_max],*env;
 
-	char vm_dir_env_str[buff_size_max], *vm_dir_env_delim = ";";
-	strcpy(vm_dir_env_str, getenv("QEMURUN_VM_PATH"));
-
-	char *vm_dir_env = strtok(vm_dir_env_str, vm_dir_env_delim);
+	strcpy(vm_dir_env_str, (const char *)((env=getenv("QEMURUN_VM_PATH"))?env:""));
+   puts(env);
+	char *vm_dir_env = strtok(vm_dir_env_str, PSEP);
 	while ( vm_dir_env != NULL && vm_dir_exists == 0 ) {
-		snprintf(vm_dir, PATH_MAX, "%s/%s", vm_dir_env, vm_name);
-		vm_dir_exists = path_is_dir(vm_dir);
-		vm_dir_env = strtok(NULL, vm_dir_env_delim);
+      dprint();
+		snprintf(vm_dir, PATH_MAX, "%s"DSEP"%s", vm_dir_env, vm_name);
+      puts(vm_dir);
+		vm_dir_exists = filetype(vm_dir,FT_PATH);
+		vm_dir_env = strtok(NULL, PSEP);
 	}
-
 	if (vm_dir_exists) {
+	   dprint();
 		char cfg_file[PATH_MAX];
-		snprintf(cfg_file, PATH_MAX, "%s/config", vm_dir);
+		snprintf(cfg_file, PATH_MAX, "%s"DSEP"config", vm_dir);
 		strcpy(out_vm_name, vm_name);
 		strcpy(out_vm_dir, vm_dir);
 		strcpy(out_vm_cfg_file, cfg_file);
 		rc = 1;
 	} else {
+	   dprint();
 		strcpy(err, "Cannot find VM, Check your QEMURUN_VM_PATH env. variable ?");
 	}
 
@@ -306,14 +325,15 @@ bool program_find_vm_location(char *err, int argc, char **argv, char *out_vm_nam
 int main(int argc, char **argv) {
 	char cmd[buff_size_max], err[buff_size_slice], str_pool_d[buff_size_max];
 	char vm_name[buff_size_slice], vm_dir[PATH_MAX-16], vm_cfg_file[buff_size_slice];
-	char *err_p = &err[0], *str_pool = &str_pool_d[0];
+	char *str_pool = &str_pool_d[0];
 	struct hashmap_s cfg;
 	puts_gpl_banner();
+	dprint();
 	if (hashmap_create(16, &cfg) == 0 &&
-		program_find_vm_location(err_p, argc, argv, vm_name, vm_dir, vm_cfg_file) &&
+		program_find_vm_location(err, argc, argv, vm_name, vm_dir, vm_cfg_file) &&
 		program_set_default_cfg_values(&cfg, &str_pool, vm_dir) &&
-		program_load_config(err_p, &cfg, &str_pool, vm_cfg_file) &&
-		program_build_cmd_line(err_p, &cfg, vm_name, cmd))
+		program_load_config(err, &cfg, &str_pool, vm_cfg_file) &&
+		program_build_cmd_line(err, &cfg, vm_name, cmd))
 	{
 		puts("Command line arguments:");
 		puts(cmd);
