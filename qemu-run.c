be77697a9a0fea430a5e30635de457bc830136d1
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-03-20 18:41:21 -0300

    Changes so it builds on FreeBSD

--- qemu-run.c
+++ qemu-run.c
@@ -23,8 +23,11 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #define __NIX__
 #define PSEP ":"
 #define DSEP "/"
-#include <sys/sysinfo.h>
+#ifdef __linux__
 #include <linux/limits.h>
+#else
+#include <limits.h>
+#endif // def Linux
 #endif
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -61,19 +64,18 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 	append_to_strpool(str_pool_ptr, val); \
 	hashmap_put_lk(cfg, lkey, val_tmp_ptr)
 
-enum {ERR_UNKOWN,ERR_ARGS,ERR_ENV,ERR_CONFIG,ERR_SYS,ERR_EXEC,ERR_ENDLIST};
-char *errs[]={
-   "Invalid argument count. Did you specified the VM Name?",
-   "Cannot find VM, Check your QEMURUN_VM_PATH env. variable ?",
-   "Cannot open config file. Check file permissions?",
-   "Invalid value for sys",
-   "There was an error trying to execute qemu. Do you have it installed?."
-};
-
-void fatal(unsigned int errcode)
-{
-   printf("There was an error in the program:\n\t%s\n",errs[errcode<ERR_ENDLIST?errcode:0]);
-   exit(1);
+enum {ERR_UNKOWN,ERR_ARGS,ERR_ENV,ERR_CONFIG,ERR_SYS,ERR_EXEC,ERR_ENDLIST,ERR_MEM_INIT};
+void fatal(unsigned int errcode) {
+	char *errs[]={
+		"Invalid argument count. Did you specified the VM Name?",
+		"Cannot find VM, Check your QEMURUN_VM_PATH env. variable?",
+		"Cannot open config file. Check file permissions?",
+		"Invalid value for sys",
+		"There was an error trying to execute qemu. Do you have it installed?",
+		"Initilization or Memory error."
+	};
+	printf("There was an error in the program:\n\t%s.\n",errs[errcode<ERR_ENDLIST?errcode:0]);
+	exit(1);
 }
 
 char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool append_endchar, char endchar) {
@@ -139,32 +141,26 @@ bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char
 	return rc;
 }
 
-bool program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpath) {
+void program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
 	dprint();
 	if (fptr == NULL)
-      fatal(ERR_CONFIG);
+	  fatal(ERR_CONFIG);
 	char line[buff_size_slice*2];
 	while(fgets(line, buff_size_slice*2, fptr) != NULL) {
 		line[strlen(line)-1] = '\0'; // Remove EOF or Newline char
 		process_kv_pair(line, cfg, str_pool, '=');
 	}
 	fclose(fptr);
-	return 1;
 }
 
-bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char *vm_dir) {
-	char path_buff[PATH_MAX], nproc_str[4], *val_tmp;
+void program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char *vm_dir) {
+	char path_buff[PATH_MAX], *val_tmp;
 	dprint();
-#ifdef __NIX__
-	snprintf(nproc_str, 4, "%d", get_nprocs());
-#else
-   strcpy(nproc_str,"2");
-#endif
 	add_cfg_val(val_tmp, str_pool, cfg, "sys", "x64");
 	add_cfg_val(val_tmp, str_pool, cfg, "efi", "no");
 	add_cfg_val(val_tmp, str_pool, cfg, "cpu", "host");
-	add_cfg_val(val_tmp, str_pool, cfg, "cores", nproc_str);
+	add_cfg_val(val_tmp, str_pool, cfg, "cores", "1");
 	add_cfg_val(val_tmp, str_pool, cfg, "mem", "2G");
 	add_cfg_val(val_tmp, str_pool, cfg, "acc", "yes");
 	add_cfg_val(val_tmp, str_pool, cfg, "vga", "virtio");
@@ -187,15 +183,13 @@ bool program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	add_cfg_val(val_tmp, str_pool, cfg, "cdrom", filetype(path_buff,FT_FILE) ? path_buff : "");
 	snprintf(path_buff, PATH_MAX, "%s/disk", vm_dir);
 	add_cfg_val(val_tmp, str_pool, cfg, "disk", filetype(path_buff,FT_FILE) ? path_buff : "");
-	return 1;
 }
 
-bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
-	bool rc = 1;
+void program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd) {
 	int drive_index = 0, telnet_port = 55555; // @TODO: Get usable TCP port
 	void* cfg_v;
 	char cmd_slice[buff_size_slice] = {0};
-   dprint();
+	dprint();
 	bool vm_has_name = (strcmp(vm_name, "") != 0 ? 1 : 0);
 	bool vm_has_acc_enabled = hashmap_match_char_lka(cfg, "acc", "yes");
 	bool vm_has_vncpwd = (strcmp(hashmap_get_lk(cfg, "vnc_pwd"), "") != 0 ? 1 : 0);
@@ -215,7 +209,7 @@ bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 	} else if (strcmp((const char*)cfg_v, "x64") == 0) {
 		out_cmd = append_to_cmd(out_cmd, "qemu-system-x86_64");
 	} else
-      fatal(ERR_SYS);
+		fatal(ERR_SYS);
 
 	snprintf(cmd_slice, buff_size_slice, "%s-name %s -cpu %s -smp %s -m %s -boot order=%s -usb -device usb-tablet -vga %s %s%s",
 		vm_has_acc_enabled ? "--enable-kvm " : "",
@@ -290,39 +284,27 @@ bool program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 
 	if (vm_has_rngdev) out_cmd = append_to_cmd(out_cmd, "-object rng-random,id=rng0,filename=/dev/random -device virtio-rng-pci,rng=rng0");
 	if (vm_clock_is_localtime) out_cmd = append_to_char_arr(out_cmd, "-rtc base=localtime", 1, 0, 0);
-	return rc;
 }
 
-bool program_find_vm_location(int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
-	bool rc = 0, vm_dir_exists = 0;
-	char *vm_name, vm_dir[PATH_MAX+1];
-
-   dprint();
-	if (argc > 1) {
-		vm_name = argv[1];
-	} else
-      fatal(ERR_ARGS);
-
-	char vm_dir_env_str[PATH_MAX*16],*env;
-
+void program_find_vm_location(int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
+	dprint();
+	if (argc < 1) fatal(ERR_ARGS);
+	char *vm_name, vm_dir[PATH_MAX+1], vm_dir_env_str[PATH_MAX*16],*env;
+	bool vm_dir_exists = 0;
+	vm_name = argv[1];
 	strcpy(vm_dir_env_str, (const char *)((env=getenv("QEMURUN_VM_PATH"))?env:""));
-
 	char *vm_dir_env = strtok(vm_dir_env_str, PSEP);
 	while ( vm_dir_env != NULL && vm_dir_exists == 0 ) {
-		snprintf(vm_dir, PATH_MAX, "%s"DSEP"%s", vm_dir_env, vm_name);
+		snprintf(vm_dir, sizeof(vm_dir), "%s"DSEP"%s", vm_dir_env, vm_name);
 		vm_dir_exists = filetype(vm_dir,FT_PATH);
 		vm_dir_env = strtok(NULL, PSEP);
 	}
-	if (vm_dir_exists) {
-		char cfg_file[PATH_MAX+1];
-		snprintf(cfg_file, PATH_MAX, "%s"DSEP"config", vm_dir);
-		strcpy(out_vm_name, vm_name);
-		strcpy(out_vm_dir, vm_dir);
-		strcpy(out_vm_cfg_file, cfg_file);
-		rc = 1;
-	} else
-      fatal(ERR_ENV);
-	return rc;
+	if (! vm_dir_exists) fatal(ERR_ENV);
+	char cfg_file[PATH_MAX+8];
+	snprintf(cfg_file, sizeof(cfg_file), "%s"DSEP"config", vm_dir);
+	strcpy(out_vm_name, vm_name);
+	strcpy(out_vm_dir, vm_dir);
+	strcpy(out_vm_cfg_file, cfg_file);
 }
 
 int main(int argc, char **argv) {
@@ -332,17 +314,14 @@ int main(int argc, char **argv) {
 	struct hashmap_s cfg;
 	dprint();
 	puts_gpl_banner();
-	if (hashmap_create(16, &cfg) == 0 &&
-		program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file) &&
-		program_set_default_cfg_values(&cfg, &str_pool, vm_dir) &&
-		program_load_config(&cfg, &str_pool, vm_cfg_file) &&
-		program_build_cmd_line(&cfg, vm_name, cmd))
-	{
-		puts("QEMU Command line arguments:");
-		puts(cmd);
-		if (system(cmd) == -1)
-         fatal(ERR_EXEC);
-	}
+	if (hashmap_create(16, &cfg) != 0) fatal(ERR_MEM_INIT);
+	program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file);
+	program_set_default_cfg_values(&cfg, &str_pool, vm_dir);
+	program_load_config(&cfg, &str_pool, vm_cfg_file);
+	program_build_cmd_line(&cfg, vm_name, cmd);
+	puts("QEMU Command line arguments:");
+	puts(cmd);
+	if (system(cmd) == -1) fatal(ERR_EXEC);
 	hashmap_destroy(&cfg);
 	return 0;
 }
