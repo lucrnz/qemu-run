14680e4f2f00a56ea0090e9075b86d5c6132a99b
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-03-20 23:32:40 -0300

    More fixes for Windows version, some codestyle, and other little changes.

--- qemu-run.c
+++ qemu-run.c
@@ -12,10 +12,9 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <strings.h>
 #include <ctype.h>
-#include <unistd.h>
 #if defined(__WIN32__) || defined(__WIN64__) || defined(__WINNT__) || defined(__DOS__)
+#define __WINDOWS__
 #define PSEP ";"
 #define DSEP "\\"
 #include <limits.h>
@@ -47,7 +46,7 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 #define hashmap_get_lk(m, lk) \
 	hashmap_get(m, lk, sizeof(lk)-1) // where k is str literal
 #define hashmap_match_char_lka(t, lk, s) \
-	(strcasecmp((const char*)hashmap_get(t, lk, sizeof(lk)-1), s) == 0 ? 1 : 0)
+	(stricmp((const char*)hashmap_get(t, lk, sizeof(lk)-1), s) == 0 ? 1 : 0)
 
 #define puts_gpl_banner() \
 	puts("qemu-run. Forever beta software. Use on production on your own risk!\nThis software is Free software - released under the GPLv3 License.\nRead the LICENSE file. Or go visit https://www.gnu.org/licenses/gpl-3.0.html\n")
@@ -64,7 +63,7 @@ along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licen
 	append_to_strpool(str_pool_ptr, val); \
 	hashmap_put_lk(cfg, lkey, val_tmp_ptr)
 
-enum {ERR_UNKOWN,ERR_ARGS,ERR_ENV,ERR_CONFIG,ERR_SYS,ERR_EXEC,ERR_ENDLIST,ERR_MEM_INIT};
+enum {ERR_UNKOWN,ERR_ARGS,ERR_ENV,ERR_CONFIG,ERR_SYS,ERR_EXEC,ERR_MEM_INIT,ERR_ENDLIST};
 void fatal(unsigned int errcode) {
 	char *errs[]={
 		"Invalid argument count. Did you specified the VM Name?",
@@ -144,11 +143,12 @@ bool process_kv_pair(char *kv_cstr, struct hashmap_s *cfg, char **str_pool, char
 void program_load_config(struct hashmap_s *cfg, char **str_pool, const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
 	dprint();
-	if (fptr == NULL)
-	  fatal(ERR_CONFIG);
+	if (fptr == NULL) { fatal(ERR_CONFIG); }
 	char line[buff_size_slice*2];
 	while(fgets(line, buff_size_slice*2, fptr) != NULL) {
-		line[strlen(line)-1] = '\0'; // Remove EOF or Newline char
+		size_t ll = strlen(line);
+		if (line[ll-1] == '\n') {line[ll-1] = '\0'; } // Truncate New line.
+		if (line[ll-1] == EOF) {line[ll-1] = '\0'; } // Truncate EOF
 		process_kv_pair(line, cfg, str_pool, '=');
 	}
 	fclose(fptr);
@@ -159,17 +159,29 @@ void program_set_default_cfg_values(struct hashmap_s *cfg, char **str_pool, char
 	dprint();
 	add_cfg_val(val_tmp, str_pool, cfg, "sys", "x64");
 	add_cfg_val(val_tmp, str_pool, cfg, "efi", "no");
+#ifdef __WINDOWS__
+	add_cfg_val(val_tmp, str_pool, cfg, "cpu", "max");
+#else
 	add_cfg_val(val_tmp, str_pool, cfg, "cpu", "host");
+#endif
 	add_cfg_val(val_tmp, str_pool, cfg, "cores", "2");
 	add_cfg_val(val_tmp, str_pool, cfg, "mem", "2G");
+#ifdef __WINDOWS__
+	add_cfg_val(val_tmp, str_pool, cfg, "acc", "no");
+#else
 	add_cfg_val(val_tmp, str_pool, cfg, "acc", "yes");
+#endif
 	add_cfg_val(val_tmp, str_pool, cfg, "vga", "virtio");
 	add_cfg_val(val_tmp, str_pool, cfg, "snd", "hda");
 	add_cfg_val(val_tmp, str_pool, cfg, "boot", "c");
 	add_cfg_val(val_tmp, str_pool, cfg, "fwd_ports", "2222:22");
 	add_cfg_val(val_tmp, str_pool, cfg, "hdd_virtio", "yes");
 	add_cfg_val(val_tmp, str_pool, cfg, "net", "virtio-net-pci");
+#ifdef __WINDOWS__
+	add_cfg_val(val_tmp, str_pool, cfg, "rng_dev", "no");
+#else
 	add_cfg_val(val_tmp, str_pool, cfg, "rng_dev", "yes");
+#endif
 	add_cfg_val(val_tmp, str_pool, cfg, "host_video_acc", "no");
 	add_cfg_val(val_tmp, str_pool, cfg, "localtime", "no");
 	add_cfg_val(val_tmp, str_pool, cfg, "headless", "no");
@@ -195,7 +207,9 @@ void program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 	bool vm_has_vncpwd = (strcmp(hashmap_get_lk(cfg, "vnc_pwd"), "") != 0 ? 1 : 0);
 	bool vm_has_audio = hashmap_match_char_lka(cfg, "snd", "no");
 	bool vm_has_videoacc = hashmap_match_char_lka(cfg, "host_video_acc", "yes");
+#ifndef __WINDOWS__
 	bool vm_has_rngdev = hashmap_match_char_lka(cfg, "rng_dev", "yes");
+#endif
 	bool vm_is_headless = hashmap_match_char_lka(cfg, "headless", "yes");
 	bool vm_clock_is_localtime = hashmap_match_char_lka(cfg, "localtime", "yes");
 	bool vm_has_sharedf = (strcmp(hashmap_get_lk(cfg, "shared"), "") != 0 ? 1 : 0);
@@ -281,8 +295,9 @@ void program_build_cmd_line(struct hashmap_s *cfg, char *vm_name, char *out_cmd)
 		out_cmd = append_to_cmd(out_cmd, cmd_slice);
 		drive_index++;
 	}
-
+#ifndef __WINDOWS__
 	if (vm_has_rngdev) out_cmd = append_to_cmd(out_cmd, "-object rng-random,id=rng0,filename=/dev/random -device virtio-rng-pci,rng=rng0");
+#endif
 	if (vm_clock_is_localtime) out_cmd = append_to_char_arr(out_cmd, "-rtc base=localtime", 1, 0, 0);
 }
 
@@ -296,12 +311,13 @@ void program_find_vm_location(int argc, char **argv, char *out_vm_name, char *ou
 	char *vm_dir_env = strtok(vm_dir_env_str, PSEP);
 	while ( vm_dir_env != NULL && vm_dir_exists == 0 ) {
 		snprintf(vm_dir, sizeof(vm_dir), "%s"DSEP"%s", vm_dir_env, vm_name);
+		printf("%s\n", vm_dir);
 		vm_dir_exists = filetype(vm_dir,FT_PATH);
 		vm_dir_env = strtok(NULL, PSEP);
 	}
-	if (! vm_dir_exists) fatal(ERR_ENV);
+	if (! vm_dir_exists) { fatal(ERR_ENV); }
 	char cfg_file[PATH_MAX+8];
-	snprintf(cfg_file, sizeof(cfg_file), "%s"DSEP"config", vm_dir);
+	snprintf(cfg_file, sizeof(cfg_file), "%s"DSEP"config", vm_dir); // @TODO: This breaks paths that are wrapped in quotes.
 	strcpy(out_vm_name, vm_name);
 	strcpy(out_vm_dir, vm_dir);
 	strcpy(out_vm_cfg_file, cfg_file);
@@ -314,14 +330,14 @@ int main(int argc, char **argv) {
 	struct hashmap_s cfg;
 	dprint();
 	puts_gpl_banner();
-	if (hashmap_create(16, &cfg) != 0) fatal(ERR_MEM_INIT);
+	if (hashmap_create(16, &cfg) != 0) { fatal(ERR_MEM_INIT); }
 	program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file);
 	program_set_default_cfg_values(&cfg, &str_pool, vm_dir);
 	program_load_config(&cfg, &str_pool, vm_cfg_file);
 	program_build_cmd_line(&cfg, vm_name, cmd);
 	puts("QEMU Command line arguments:");
 	puts(cmd);
-	if (system(cmd) == -1) fatal(ERR_EXEC);
+	if (system(cmd) == -1) { fatal(ERR_EXEC); }
 	hashmap_destroy(&cfg);
 	return 0;
 }
