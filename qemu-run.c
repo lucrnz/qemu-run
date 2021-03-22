ac8762d873ab8de882493b350bd02001883b9699
Lucie <lucie_linux@protonmail.com>
2021-03-22 06:37:30 -0300

    Code style & Made printf with IFDEF Debug

--- qemu-run.c
+++ qemu-run.c
@@ -8,6 +8,7 @@ WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with qemu-run; see the file LICENSE.  If not see <http://www.gnu.org/licenses/>.*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -69,43 +70,37 @@ void fatal(unsigned int errcode) {
 		"There was an error trying to execute qemu. Do you have it installed?",
 		"Initilization or Memory error."
 	};
-   dprint();
+	dprint();
 	printf("There was an error in the program:\n\t%s.\n",errs[errcode<ERR_ENDLIST?errcode:0]);
 	exit(1);
 }
 
-long sym_hash_generate(char *str)
-{
-   int pos=0;
-   long hash=0xCAFEBABE;
-
-   while(str[pos]) {
-      hash=~(((((hash&0xFF)^str[pos])<<5)|
-         (((hash>>26)&0x1f)^(pos&31)))|
-         (hash<<18));
-      pos++;
-   }
-   return hash;
+long sym_hash_generate(char *str) {
+	int pos=0; long hash=0xCAFEBABE;
+	while(str[pos]) {
+		hash=~(((((hash&0xFF)^str[pos])<<5)|
+			(((hash>>26)&0x1f)^(pos&31)))|
+			(hash<<18));
+		pos++;
+	}
+	return hash;
 }
 
-bool sym_put_kv(char *key,char *val)
-{
-   long hash,cnt=0,ret=0;
-
-   hash=sym_hash_generate(key);
-   while(cnt<KEY_ENDLIST) {
-      if(hash==cfg[cnt].hash) {
-         ret=1;
-         cfg[cnt].val=strdup(val);
-      }
-      cnt++;
-   }
-   return ret;
+bool sym_put_kv(char *key,char *val) {
+	long hash,cnt=0,ret=0;
+	hash=sym_hash_generate(key);
+	while(cnt<KEY_ENDLIST) {
+		if(hash==cfg[cnt].hash) {
+			ret=1;
+			cfg[cnt].val=strdup(val);
+		}
+		cnt++;
+	}
+	return ret;
 }
 
 char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool append_endchar, char endchar) {
 	char c; bool stop = 0;
-
 	dprint();
 	while (!stop) {
 		c = *src; src++;
@@ -120,7 +115,6 @@ char *append_to_char_arr(char *dst, const char *src, bool append_strterm, bool a
 enum {FT_TYPE,FT_UNKNOWN=0,FT_PATH,FT_FILE};
 int filetype(const char *fpath,int type) {
 	int ret = 0; struct stat sb;
-
 	if ((ret=(access (fpath,0) == 0))) {
 		stat(fpath,&sb);
 		ret=S_ISREG(sb.st_mode)>0?FT_FILE:(S_ISDIR(sb.st_mode)?FT_PATH:FT_UNKNOWN);
@@ -130,8 +124,7 @@ int filetype(const char *fpath,int type) {
 
 char* cstr_remove_quotes(char* c) {
 	size_t len = strlen(c);
-
-   dprint();
+	dprint();
 	if (c[len-1] == '\"' && c[0] == '\"') {
 		c[len-1] = '\0';
 		return c+1;
@@ -140,7 +133,6 @@ char* cstr_remove_quotes(char* c) {
 
 size_t cstr_trim_right(const char *cstr, const size_t len) {
 	size_t diff = 0;
-
 	dprint();
 	for (size_t i = len - 1; i < len; i--) {
 		if (! isspace((int)cstr[i])) break;
@@ -152,22 +144,22 @@ size_t cstr_trim_right(const char *cstr, const size_t len) {
 #ifdef __WINDOWS__ // For now I only need this function on Windows.
 bool get_binary_full_path(const char *bin_fname, char *out_bin_fpath, char *out_dir) {
 	bool found = 0;
-    char fpath_b[PATH_MAX], env_b[PATH_MAX*16], *env;
-    strcpy(env_b, (const char *)((env=getenv("PATH"))?env:""));
-    char *dir_p = strtok(env_b, PSEP);
+	char fpath_b[PATH_MAX], env_b[PATH_MAX*16], *env;
+	strcpy(env_b, (const char *)((env=getenv("PATH"))?env:""));
+	char *dir_p = strtok(env_b, PSEP);
 
-    dprint();
-    while (dir_p && !found) {
-    	char* dir_pq = cstr_remove_quotes(dir_p);
-        snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE);
-        if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.exe", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
-        if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.com", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
-        if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.bat", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
-        if (found && out_dir) { strcpy(out_dir, dir_pq); }
-        dir_p = strtok(NULL, PSEP);
-    }
-    if (found && out_bin_fpath) { strcpy(out_bin_fpath, fpath_b); }
-    return found;
+	dprint();
+	while (dir_p && !found) {
+		char* dir_pq = cstr_remove_quotes(dir_p);
+		snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE);
+		if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.exe", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
+		if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.com", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
+		if (!found) { snprintf(fpath_b, sizeof(fpath_b), "%s"DSEP"%s.bat", dir_pq, bin_fname); found = filetype(fpath_b, FT_FILE); }
+		if (found && out_dir) { strcpy(out_dir, dir_pq); }
+		dir_p = strtok(NULL, PSEP);
+	}
+	if (found && out_bin_fpath) { strcpy(out_bin_fpath, fpath_b); }
+	return found;
 }
 #endif
 
@@ -177,46 +169,50 @@ bool process_kv_pair(char *kv_cstr, char delim) {
 	size_t val_len = 0, key_len = 0;
 
 	dprint();
+#ifdef DEBUG
 	printf("process_kv_pair(\"%s\", '%c' (%d)))\n",kv_cstr,delim,delim);
+#endif
 	if((del_ptr=strchr(kv_cstr,delim))) {
-      key_ptr=kv_cstr;
-      while(key_ptr<del_ptr) {   // Adjust pointer, skip spaces
-         if(!(*key_ptr==' ' || *key_ptr=='\t')) break;
-         key_ptr++;
-      }
-      val_ptr=del_ptr+1;
-      while(--del_ptr>=key_ptr) { // Adjust end pointer, skip spaces
-         if(!(*del_ptr==' ' || *del_ptr=='\t')) break;
-      }
-      ++del_ptr;
-      key_len=(del_ptr-key_ptr);
-      while(*val_ptr) {
-         if(!(*val_ptr==' ' || *val_ptr=='\t')) break;
-         val_ptr++;
-      }
-      val_len=0;
-      while(val_ptr[val_len]) val_len++;
-      while(val_len) {
-         if(!(val_ptr[val_len-1]==' ' || val_ptr[val_len-1]=='\t')) break;
-         --val_len;
-      }
+		key_ptr=kv_cstr;
+		while(key_ptr<del_ptr) {   // Adjust pointer, skip spaces
+			if(!(*key_ptr==' ' || *key_ptr=='\t')) break;
+			key_ptr++;
+		}
+		val_ptr=del_ptr+1;
+		while(--del_ptr>=key_ptr) { // Adjust end pointer, skip spaces
+			if(!(*del_ptr==' ' || *del_ptr=='\t')) break;
+		}
+		++del_ptr;
+		key_len=(del_ptr-key_ptr);
+		while(*val_ptr) {
+			if(!(*val_ptr==' ' || *val_ptr=='\t')) break;
+			val_ptr++;
+		}
+		val_len=0;
+		while(val_ptr[val_len]) val_len++;
+		while(val_len) {
+			if(!(val_ptr[val_len-1]==' ' || val_ptr[val_len-1]=='\t')) break;
+			--val_len;
+		}
 	}
 	if(key_len) {
-      strncpy(key_buff,key_ptr,key_len);
-      if(val_len)
-         strncpy(val_buff,val_ptr,val_len);
-      else
-         val_buff[0]=0;
-      rc=sym_put_kv(key_buff,val_buff);
+		strncpy(key_buff,key_ptr,key_len);
+		if (val_len) {
+			strncpy(val_buff,val_ptr,val_len);
+		} else {
+			val_buff[0]=0;
+		}
+		rc=sym_put_kv(key_buff,val_buff);
 	}
 	return rc;
 }
 
 void program_load_config(const char *fpath) {
 	FILE *fptr = fopen(fpath, "r");
-
 	dprint();
-	printf("%s\n",fpath);
+#ifdef DEBUG
+	printf("fpath=%s\n",fpath);
+#endif
 	if (!fptr) { fatal(ERR_CONFIG); }
 	char line[LINE_AVG*2];
 	while(fgets(line, LINE_AVG*2, fptr)) {
@@ -230,7 +226,6 @@ void program_load_config(const char *fpath) {
 
 void program_set_default_cfg_values(char *vm_dir) {
 	char path_buff[PATH_MAX];
-
 	dprint();
 #ifdef __WINDOWS__
 	sym_put_kv("acc", "no");
@@ -250,7 +245,7 @@ void program_set_default_cfg_values(char *vm_dir) {
 void program_build_cmd_line(char *vm_name, char *out_cmd) {
 	int drive_index = 0, telnet_port = 55555; // @TODO: Get usable TCP port
 	char cmd_slice[LINE_MAX] = {0};
-   dprint();
+	dprint();
 #ifdef __WINDOWS__
 	char* cmd_sp = out_cmd; // Need this variable, for a Windows fix..
 #else
@@ -267,8 +262,7 @@ void program_build_cmd_line(char *vm_name, char *out_cmd) {
 	bool vm_has_hddvirtio = stricmp(cfg[KEY_HDD_VIRTIO].val, "yes")==0;
 	bool vm_has_network = (strcmp(cfg[KEY_NET].val, "") != 0 );
 	vm_has_sharedf = vm_has_sharedf ? filetype(cfg[KEY_SHARED].val,FT_PATH) : 0;
-
-   *out_cmd=0;
+	*out_cmd=0;
 	if (strcmp(cfg[KEY_SYS].val, "x32") == 0) {
 		out_cmd = strcpy(out_cmd, "qemu-system-i386");
 	} else if (strcmp(cfg[KEY_SYS].val, "x64") == 0) {
@@ -347,7 +341,7 @@ void program_build_cmd_line(char *vm_name, char *out_cmd) {
 	* Otherwise it wont find the BIOS file.. */
 	char q_fp[LINE_AVG], q_fn[LINE_AVG], wfix_arg[LINE_AVG+6]={0};
 	size_t q_fn_l = 0;
-   dprint();
+	dprint();
 	for (; cmd_sp[q_fn_l] && cmd_sp[q_fn_l] != ' '; q_fn_l++) { q_fn[q_fn_l] = cmd_sp[q_fn_l]; } // Copy the qemu exe file name
 	q_fn[q_fn_l] = 0;
 	if (! get_binary_full_path(q_fn, NULL, &q_fp[0])) { fatal(ERR_EXEC); } // Grab Qemu EXE file path, using PATH env.
@@ -362,7 +356,6 @@ void program_build_cmd_line(char *vm_name, char *out_cmd) {
 void program_find_vm_location(int argc, char **argv, char *out_vm_name, char *out_vm_dir, char *out_vm_cfg_file) {
 	char *vm_name, vm_dir[PATH_MAX+1], vm_dir_env_str[PATH_MAX*16],*env;
 	bool vm_dir_exists = 0;
-
 	dprint();
 	if (argc < 1) { fatal(ERR_ARGS); }
 	vm_name = argv[1];
@@ -384,14 +377,15 @@ void program_find_vm_location(int argc, char **argv, char *out_vm_name, char *ou
 int main(int argc, char **argv) {
 	char cmd[LINE_MAX];
 	char vm_name[LINE_AVG], vm_dir[PATH_MAX+1], vm_cfg_file[LINE_AVG];
-
 	dprint();
 	puts_gpl_banner();
 	program_find_vm_location(argc, argv, vm_name, vm_dir, vm_cfg_file);
-   program_set_default_cfg_values(vm_dir);
-   program_load_config(vm_cfg_file);
-   for(int i=0;i<KEY_ENDLIST;i++) printf("%d %X='%s'\n",i,cfg[i].hash,cfg[i].val);
-   program_build_cmd_line(vm_name, cmd);
+	program_set_default_cfg_values(vm_dir);
+	program_load_config(vm_cfg_file);
+#ifdef DEBUG
+	puts("Hash table:"); for(int i=0;i<KEY_ENDLIST;i++) printf("%d %X='%s'\n",i,cfg[i].hash,cfg[i].val);
+#endif
+	program_build_cmd_line(vm_name, cmd);
 	puts("QEMU Command line arguments:");
 	puts(cmd);
 	return system(cmd); // Just Pass Qemu's Err code on
